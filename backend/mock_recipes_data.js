70c44a51c652b085dac1c660a26a5344e61d3270
John Salinas <46495232+JohnSalinas123@users.noreply.github.com>
2023-11-16 08:55:52 -0800

    moved recipe mock data to file

---
+++ backend/mock_recipes_data.js
@@ -0,0 +1,279 @@
+export let testRecipe = 
+[
+    {
+        "_id" : 1,
+        "name": "Burger",
+        "description" : "This is a burger recipe",
+        "vegetarian" : false,
+        "ingredients": [
+            {
+                "name": "Beef Patty",
+                "category" : "protein",
+            },
+            {
+                "name" : "Cheddar Cheese",
+                "category" : "dairy"
+            },
+            {
+                "name" : "Lettuce",
+                "category": "vegetable"
+            }
+        ],
+        "steps": [
+            {
+                "step_num": 1,
+                "instruction": "Pan dry beef patty on stove until cooked to liking"
+            },
+            {
+                "step_num": 2,
+                "instruction": "Add cheddar cheese slice on cooking beef patty, remove patty when cheese is melted"
+            }
+        ]
+    },
+    {
+        "_id" : 2,
+        "name": "Pizza",
+        "description" : "This is a pizza recipe",
+        "ingredients": [
+            {
+                "name": "Pizza Dough",
+                "category" : "Carb",
+            },
+            {
+                "name" : "Tomato Sauce",
+                "category" : "Vegetable"
+            },
+            {
+                "name" : "Mozarella Cheese",
+                "category": "Dairy"
+            },
+            {
+                "name" : "Pepperoni",
+                "category": "Protein"
+            }
+        ],
+        "steps": [
+            {
+                "step_num": 1,
+                "instruction": "Flatten dough into base of pizza"
+            },
+            {
+                "step_num": 2,
+                "instruction": "Spread tomato sauce on pizza dough base"
+            },
+            {
+                "step_num": 3,
+                "instruction": "Add mozarella cheese"
+            },
+            {
+                "step_num": 4,
+                "instruction": "Add pepperoni"
+            }
+        ]
+    },
+    {
+        "_id" : 3,
+        "name": "Pizza",
+        "description" : "This is a pizza recipe",
+        "ingredients": [
+            {
+                "name": "Pizza Dough",
+                "category" : "Carb",
+            },
+            {
+                "name" : "Tomato Sauce",
+                "category" : "Vegetable"
+            },
+            {
+                "name" : "Mozarella Cheese",
+                "category": "Dairy"
+            },
+            {
+                "name" : "Pepperoni",
+                "category": "Protein"
+            }
+        ],
+        "steps": [
+            {
+                "step_num": 1,
+                "instruction": "Flatten dough into base of pizza"
+            },
+            {
+                "step_num": 2,
+                "instruction": "Spread tomato sauce on pizza dough base"
+            },
+            {
+                "step_num": 3,
+                "instruction": "Add mozarella cheese"
+            },
+            {
+                "step_num": 4,
+                "instruction": "Add pepperoni"
+            }
+        ]
+    },
+    {
+        "_id" : 4,
+        "name": "Pizza",
+        "description" : "This is a pizza recipe",
+        "ingredients": [
+            {
+                "name": "Pizza Dough",
+                "category" : "Carb",
+            },
+            {
+                "name" : "Tomato Sauce",
+                "category" : "Vegetable"
+            },
+            {
+                "name" : "Mozarella Cheese",
+                "category": "Dairy"
+            },
+            {
+                "name" : "Pepperoni",
+                "category": "Protein"
+            }
+        ],
+        "steps": [
+            {
+                "step_num": 1,
+                "instruction": "Flatten dough into base of pizza"
+            },
+            {
+                "step_num": 2,
+                "instruction": "Spread tomato sauce on pizza dough base"
+            },
+            {
+                "step_num": 3,
+                "instruction": "Add mozarella cheese"
+            },
+            {
+                "step_num": 4,
+                "instruction": "Add pepperoni"
+            }
+        ]
+    },
+    {
+        "_id" : 5,
+        "name": "Pizza",
+        "description" : "This is a pizza recipe",
+        "ingredients": [
+            {
+                "name": "Pizza Dough",
+                "category" : "Carb",
+            },
+            {
+                "name" : "Tomato Sauce",
+                "category" : "Vegetable"
+            },
+            {
+                "name" : "Mozarella Cheese",
+                "category": "Dairy"
+            },
+            {
+                "name" : "Pepperoni",
+                "category": "Protein"
+            }
+        ],
+        "steps": [
+            {
+                "step_num": 1,
+                "instruction": "Flatten dough into base of pizza"
+            },
+            {
+                "step_num": 2,
+                "instruction": "Spread tomato sauce on pizza dough base"
+            },
+            {
+                "step_num": 3,
+                "instruction": "Add mozarella cheese"
+            },
+            {
+                "step_num": 4,
+                "instruction": "Add pepperoni"
+            }
+        ]
+    },
+    {
+        "_id" : 6,
+        "name": "Pizza",
+        "description" : "This is a pizza recipe",
+        "ingredients": [
+            {
+                "name": "Pizza Dough",
+                "category" : "Carb",
+            },
+            {
+                "name" : "Tomato Sauce",
+                "category" : "Vegetable"
+            },
+            {
+                "name" : "Mozarella Cheese",
+                "category": "Dairy"
+            },
+            {
+                "name" : "Pepperoni",
+                "category": "Protein"
+            }
+        ],
+        "steps": [
+            {
+                "step_num": 1,
+                "instruction": "Flatten dough into base of pizza"
+            },
+            {
+                "step_num": 2,
+                "instruction": "Spread tomato sauce on pizza dough base"
+            },
+            {
+                "step_num": 3,
+                "instruction": "Add mozarella cheese"
+            },
+            {
+                "step_num": 4,
+                "instruction": "Add pepperoni"
+            }
+        ]
+    },
+    {
+        "_id" : 7,
+        "name": "Pizza",
+        "description" : "This is a pizza recipe",
+        "ingredients": [
+            {
+                "name": "Pizza Dough",
+                "category" : "Carb",
+            },
+            {
+                "name" : "Tomato Sauce",
+                "category" : "Vegetable"
+            },
+            {
+                "name" : "Mozarella Cheese",
+                "category": "Dairy"
+            },
+            {
+                "name" : "Pepperoni",
+                "category": "Protein"
+            }
+        ],
+        "steps": [
+            {
+                "step_num": 1,
+                "instruction": "Flatten dough into base of pizza"
+            },
+            {
+                "step_num": 2,
+                "instruction": "Spread tomato sauce on pizza dough base"
+            },
+            {
+                "step_num": 3,
+                "instruction": "Add mozarella cheese"
+            },
+            {
+                "step_num": 4,
+                "instruction": "Add pepperoni"
+            }
+        ]
+    }
+]
