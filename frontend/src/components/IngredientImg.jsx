95fc4c754fe9037bdab22db7ab83d45d0aaa8d32
Huijun Hu <huijunhu@HuijundeiMac.local>
2023-11-21 00:41:25 -0800

    less ugly layout

---
+++ frontend/src/components/IngredientImg.jsx
@@ -0,0 +1,17 @@
+// const getImg = function(key) {
+//     let imgSrc;
+//     switch (key){
+//       case 'carbohydrate' :
+//         imgSrc = "../resources/carb_ingredient.png";
+//       case 'protein' :
+//         imgSrc = "../resources/protein_ingredient.png";
+//       case 'vegetable' :
+//         imgSrc = "../resources/vege_ingredient.png";
+//       case 'fat' :
+//         imgSrc = "../resources/fat_ingredient.png";
+//       case 'fruit' :
+//         imgSrc = "../resources/fruit_ingredient.png";
+//     }
+//   }
+
+//   export default getImg;
--- frontend/src/components/IngredientList.jsx
+++ frontend/src/components/IngredientList.jsx
@@ -1,28 +1,39 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+import "../styles/ingredient.css"
+import "../styles/general.css"
+
 axios.defaults.baseURL = 'http://localhost:8080/';
 
 export const IngredientsList = () => {
   const [ingredients, setIngredients] = useState([]);
 
   useEffect(() => {
-    axios.get('api/ingredients')
+    try{
+      const {ingredientArr} = axios.get('api/ingredients')
       .then(response => setIngredients(response.data))
       .catch(error => console.error(error));
+  
+    }catch(err){
+      console.error(err);
+    }
   }, []);
+    
 
   return (
-    <div>
-      <h1>Ingredients</h1>
-      <ul>
-        {ingredients.map(ingredients => (
-          <li key={ingredients._id}>
-            <h3>{ingredients.name}</h3>
-            <p>{ingredients.calories}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <><div class="grid-container">
+
+      {ingredients.map(ingredients => (
+        <div class="item-container" key={ingredients._id}>
+                    
+          <h3>{ingredients.name}</h3>
+          <p>{ingredients.category}</p>
+          <p>{ingredients.calories}</p>
+          
+        </div>
+      ))}
+    </div><p>You Reach The End</p></>
+    
   );
 }
