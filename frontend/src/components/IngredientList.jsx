4069931072b48925331170cf62483606eecd061f
Huijun Hu <huijunhu@HuijundeiMac.local>
2023-11-21 01:01:56 -0800

    modify ingredientList

--- frontend/src/components/IngredientList.jsx
+++ frontend/src/components/IngredientList.jsx
@@ -3,6 +3,9 @@ import axios from "axios";
 
 import "../styles/ingredient.css"
 import "../styles/general.css"
+import carb from '../resources/carb_ingredient.png'
+import protein from '../resources/protein_ingredient.png'
+//import getImg from "./IngredientImg";
 
 axios.defaults.baseURL = 'http://localhost:8080/';
 
@@ -26,7 +29,7 @@ export const IngredientsList = () => {
 
       {ingredients.map(ingredients => (
         <div class="item-container" key={ingredients._id}>
-                    
+          <img src={protein} height = {200} width = {200}></img>
           <h3>{ingredients.name}</h3>
           <p>{ingredients.category}</p>
           <p>{ingredients.calories}</p>
