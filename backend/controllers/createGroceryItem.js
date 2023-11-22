de62fd8c06a5f5ce44497ab45ea6b465513c9672
RashadKhan96 <Rashadkhan96@gmail.com>
2023-11-22 12:30:43 -0800

    got createGroceryItem function to work

--- backend/controllers/createGroceryItem.js
+++ backend/controllers/createGroceryItem.js
@@ -1,10 +1,12 @@
+
+
+
 import GroceryItem from "../models/groceryItemModel.js";
 
 export const createGroceryItem = async (req, res) => {
     try {
         const newGroceryItem = new GroceryItem(req.body);
         const savedGroceryItem = await newGroceryItem.save();
-
         console.log("Created Grocery Item!");
         res.status(201).json(savedGroceryItem);
     } catch (error) {
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { createUser } from '../controllers/createUser.js';
 import { getUserMealPlans } from '../controllers/getUserMealPlans.js';
 import { getGroceryItems } from '../controllers/getGroceryItems.js';
+import { createGroceryItem } from '../controllers/createGroceryItem.js';
 //import { loginUser } from '../controllers/loginUser.js';
 
 const router = express.Router();
@@ -14,6 +15,8 @@ router.post('/', createUser);
 
 //get: grocery items related to users object _Id
 router.get('/getGroceryItem/:userId', getGroceryItems);
+// post: creating a new grocery item
+router.post('/groceryItems', createGroceryItem);
 
 // get: retrieve user meal plans
 // post: create user meal plan
