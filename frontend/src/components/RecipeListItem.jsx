641729ac2e3718b64dd70ccedd23bceb1e47b53d
JohnSalinas123 <salinasjohn257@gmail.com>
2023-11-30 13:39:19 -0800

    changed design of recipe cards, add vege img

--- frontend/src/components/RecipeListItem.jsx
+++ frontend/src/components/RecipeListItem.jsx
@@ -2,7 +2,7 @@ import Accordion from 'react-bootstrap/Accordion'
 import Card from 'react-bootstrap/Card'
 import "../styles/recipe.css"
 import "../styles/general.css"
-import RecipeImage from "../resources/recipe_pixel_art.jpg"
+import burger_meal from "../resources/burger_meal.png"
 import Col from "react-bootstrap/Col"
 
 export const RecipeListItem = (props) => {
@@ -12,7 +12,7 @@ export const RecipeListItem = (props) => {
     return (
       <Col xs={12} sm={6} md={4} lg={3}>
         <Card style={{ width: '18rem' }} className="recipe-item-container">
-          <Card.Img className="recipe-image center" variant="top" src={RecipeImage} />
+          <Card.Img className="recipe-image center" variant="top" src={burger_meal} />
           <Card.Body className='recipe-body'>
             <Card.Title>{props.name}</Card.Title>
             <Card.Text>{props.cal} cal</Card.Text>
