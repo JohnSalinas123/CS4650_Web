d47a3f77b941bf738779e214f1713a7f89b87924
John Salinas <46495232+JohnSalinas123@users.noreply.github.com>
2023-12-03 17:54:06 -0800

    add ingredient form field wip

--- frontend/src/components/RecipesForm.jsx
+++ frontend/src/components/RecipesForm.jsx
@@ -2,15 +2,38 @@ import { useState, useEffect } from 'react'
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal'; 
 import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import { MultiSelect } from 'react-multi-select-component';
 import axios from 'axios';
+import '../styles/recipe.css'
+
+
+const RecipeIngredientRow = (props) => {
+
+    return (
+        <div>
+
+        </div>
+    )
+
+}
+
 
 export const RecipeForm = (props) => {
     const [recipeName, setRecipeName] = useState('');
     const [recipeDescrip, setRecipeDescrip] = useState('');
     const [recipeCal, setRecipeCal] = useState('');
     const [recipeDiet, setRecipeDiet] = useState('');
-    const [recipeIngreds, setRecipeIngreds] = useState([]);
+    const [recipeIngreds, setRecipeIngreds] = useState([
+        {
+            "index": 1,
+            "name": "",
+            "quantity": "",
+            "unit" : "grams"
+        }
+    ]);
+    const [recipeIngredsCount, setRecipeIngredsCount] = useState(1);
     const [recipeSteps, setRecipeSteps] = useState([]);
 
     axios.defaults.baseURL = 'http://localhost:8080/';
@@ -26,6 +49,53 @@ export const RecipeForm = (props) => {
           console.error(err);
         }
       }, []);
+
+    const addIngredientRow = () => {
+
+        let recipeCount = recipeIngredsCount + 1
+
+        setRecipeIngredsCount(recipeCount)
+
+        const newIngredientRow = {
+            index: recipeCount,
+            name: "",
+            quantity: "",
+            unit: "grams"
+
+        }
+        setRecipeIngreds((prevIngreds) => [...prevIngreds, newIngredientRow]);
+        console.log(recipeIngreds)
+    }
+
+    const renderIngredientRows = () => {
+        console.log("ADD INGREDIENT ROW")
+        return recipeIngreds.map((ingredient, index) => (
+            <div>
+                <Row key={index} className="recipe-ingred-row">
+                    <Col xs={1} className="text-center">
+                        
+                            {ingredient.index}.
+        
+                    </Col>
+                    <Col xs={5}>
+                        <Form.Control placeholder="Name" default={ingredient.name} />
+                    </Col>
+                    <Col>
+                        <Form.Control placeholder="Quantity" value={ingredient.quantity}/>
+                    </Col>
+                    <Col>
+                        <Form.Select defaultValue="Diet..." value={ingredient.unit}>
+                            <option>ml</option>
+                            <option>grams</option>
+                            <option>cups</option>
+                            <option>oz</option>
+                        </Form.Select>
+                    </Col>    
+                </Row>
+            </div>
+        ));
+
+    }
 
     const saveInput = (event) => {
 		// save input in field
@@ -49,58 +119,72 @@ export const RecipeForm = (props) => {
 	}; // end saveInput const
 
     return (
-        <Modal centered show={props.show} onHide={props.handleClose}>
-            <Modal.Header closeButton>
-            <Modal.Title>Create a new recipe!</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form>
-                    <Form.Group className='mb-3' controlId='recipeName' onChange={saveInput}>
-                        <Form.Label> Name </Form.Label>
-                        <Form.Control placeholder='Ex: Pepperoni Pizza' />
-                    </Form.Group>
+            <Modal size="lg" centered show={props.show} onHide={props.handleClose}>
+                <Modal.Header closeButton>
+                <Modal.Title>Create a new recipe!</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Row>
+                            <Col>
+                                <Form.Group className='mb-3' controlId='recipeName' onChange={saveInput}>
+                                    <Form.Label> Name </Form.Label>
+                                    <Form.Control placeholder='Ex: Pepperoni Pizza' />
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                <Form.Group className='mb-3' controlId='recipeCalories' onChange={saveInput}>
+                                    <Form.Label> Calories </Form.Label>
+                                    <Form.Control placeholder='Ex. 500' />
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                <Form.Group className='mb-3' controlId='recipeDiet' onChange={saveInput}>
+                                    <Form.Label> Diet </Form.Label>
+                                    <Form.Select defaultValue="Diet...">
+                                        <option>Omnivore</option>
+                                        <option>Vegetarian</option>
+                                        <option>Vegan</option>
+                                        <option>Keto</option>
+                                    </Form.Select>
+                                </Form.Group>
+                            </Col>
 
-                    <Form.Group className='mb-3' controlId='recipeDescrip' onChange={saveInput}>
-                        <Form.Label> Description </Form.Label>
-                        <Form.Control as='textarea' rows={2} />
-                    </Form.Group>
+                        </Row>
 
-                    <Form.Group className='mb-3' controlId='recipeCalories' onChange={saveInput}>
-                        <Form.Label> Calories </Form.Label>
-                        <Form.Control placeholder='Ex. 500' />
-                    </Form.Group>
+                        <Form.Group className='mb-3' controlId='recipeDescrip' onChange={saveInput}>
+                            <Form.Label> Description </Form.Label>
+                            <Form.Control as='textarea' rows={2} />
+                        </Form.Group>
+                        
+                        <Form.Group className='mb-3' controlId='recipeIngredients' onChange={saveInput}>
+                            <Form.Label> Ingredients </Form.Label>
+                            <Form>
+                                {renderIngredientRows()}
+                                <Row>
+                                    <Col className="d-grid">
+                                        <Button  onClick={addIngredientRow}>Add</Button>
+                                    </Col>
+                                </Row>    
+                            </Form>
+                        </Form.Group>
 
-                    <Form.Group className='mb-3' controlId='recipeDiet' onChange={saveInput}>
-                        <Form.Label> Diet </Form.Label>
-                        <Form.Select defaultValue="Diet...">
-                            <option>Omnivore</option>
-                            <option>Vegetarian</option>
-                            <option>Vegan</option>
-                            <option>Keto</option>
-                        </Form.Select>
-                    </Form.Group>
-                    
-                    <Form.Group className='mb-3' controlId='recipeIngredients' onChange={saveInput}>
-                        <Form.Label> Ingredients </Form.Label>
-                        <Form.Control as='textarea' rows={2} />
-                    </Form.Group>
+                        <Form.Group className='mb-3' controlId='recipeSteps' onChange={saveInput}>
+                            <Form.Label> Steps </Form.Label>
+                            <Form.Control as='textarea' rows={2} />
+                        </Form.Group>
 
-                    <Form.Group className='mb-3' controlId='recipeSteps' onChange={saveInput}>
-                        <Form.Label> Steps </Form.Label>
-                        <Form.Control as='textarea' rows={2} />
-                    </Form.Group>
-
-                </Form>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={props.handleClose}>
-                    Close
-                </Button>
-                <Button variant="primary" onClick={props.handleClose}>
-                    Save Recipe
-                </Button>
-            </Modal.Footer>
-      </Modal>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={props.handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={props.handleClose}>
+                        Save Recipe
+                    </Button>
+                </Modal.Footer>
+        </Modal>
     )
 
 }
