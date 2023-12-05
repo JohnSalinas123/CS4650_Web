0b54a42218f220278c64bd56418408e06295aafd
Huijun Hu <huijunhu@HuijundeiMac.local>
2023-12-04 23:54:14 -0800

    ingredient form show on add-button click

--- frontend/src/components/IngredientForm.jsx
+++ frontend/src/components/IngredientForm.jsx
@@ -1,13 +1,75 @@
-import React from "react";
+import React, {useState} from 'react';
+import axios from 'axios';
+import '../styles/ingredient.css'
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal'; 
+import Button from 'react-bootstrap/Button';
 
-function onclick(){
-    const[val,setVal]=useState([])
+export const IngredientForm = (props) => {
+    const [ingredientName, setRecipeName] = useState('');
+    const [ingredientCategory, setRecipeDescrip] = useState('');
+    const [ingredientCalories, setRecipeCal] = useState('');    
 
-    return(
-        <>
+    const handleButtonClick = () => {
+            setShowForm(true);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    const saveInput = (event) => {
+		const eventField = event.target.id;
+
+		switch (eventField) {
+			case 'ingredientName': // uses control ID
+				setRecipeName(event.target.value);
+				break;
+			case 'ingredientCategory':
+				setRecipeDescrip(event.target.value);
+            case 'ingredientCalories':
+                setRecipeCal(event.target.value)
+				break;
+			default:
+				console.log('Error: no specified field found');
+		} // end switch
+	};
+
+    return (
+        <Modal size="lg" centered show={props.show} onHide={props.handleClose} >
+                <Modal.Header closeButton>
+                <Modal.Title>Add New Ingredient</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+
+                </Modal.Body>
+                <Form  show={props.show} onHide={props.handleClose} onSubmit={handleSubmit} style={{maxWidth: '200px', margin: 'auto' }}>
+                    <Col>
+                        <Form.Group className='mb-3' controlId='ingredientName' onChange={saveInput}>
+                            <Form.Label> Name </Form.Label>
+                            <Form.Control placeholder='Ex: Kiwi' />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group className='mb-3' controlId='ingredientCategory' onChange={saveInput}>
+                            <Form.Label> Category </Form.Label>
+                            <Form.Control placeholder='Ex: Fruit' />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group className='mb-3' controlId='ingredientCalories' onChange={saveInput}>
+                            <Form.Label> Calories </Form.Label>
+                            <Form.Control placeholder='Ex: 42' />
+                        </Form.Group>
+                    </Col>
+                    <Modal.Footer>
+                        <Button onClick={props.handleClose}>
+                            Add
+                        </Button>
+                    </Modal.Footer>
+                </Form>
         
-        </>
-    )
-}
-
-export defual onclick;
+        </Modal>
+    );
+};
--- frontend/src/components/IngredientList.jsx
+++ frontend/src/components/IngredientList.jsx
@@ -1,81 +1,97 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-import "../styles/ingredient.css"
-import "../styles/general.css"
+import "../styles/ingredient.css";
+import "../styles/general.css";
 
-//import {IngredientForm} from './IngredientForm'
-import carb from '../resources/carb_ingredient.png'
-import protein from '../resources/protein_ingredient.png'
-import fat from '../resources/fat_ingredient.png'
-import vege from '../resources/vege_ingredient.png'
-import fruit from '../resources/fruit_ingredient.png'
-import random from '../resources/defualt_ingredient.png'
-import addButton from '../resources/addButton.png'
+import {IngredientForm} from './IngredientForm';
+import carb from '../resources/carb_ingredient.png';
+import protein from '../resources/protein_ingredient.png';
+import fat from '../resources/fat_ingredient.png';
+import vege from '../resources/vege_ingredient.png';
+import fruit from '../resources/fruit_ingredient.png';
+import random from '../resources/defualt_ingredient.png';
+import addButton from '../resources/addButton.png';
+import Col from 'react-bootstrap/Col';
 
 axios.defaults.baseURL = 'http://localhost:8080/';
 
 export const IngredientsList = () => {
-  const [ingredients, setIngredients] = useState([]);
+	const [ingredients, setIngredients] = useState([]);
+	const [showingForm, setShowingForm] = useState(false)
+	
+	useEffect(() => {
+		try{
+		const {ingredientArr} = axios.get('api/ingredients')
+		.then(response => setIngredients(response.data))
+		.catch(error => console.error(error));
+	
+		}catch(err){
+		console.error(err);
+		}
+	}, []);
+		
+	const handleShowForm = () => setShowingForm(true);
 
-  useEffect(() => {
-    try{
-      const {ingredientArr} = axios.get('api/ingredients')
-      .then(response => setIngredients(response.data))
-      .catch(error => console.error(error));
-  
-    }catch(err){
-      console.error(err);
-    }
-  }, []);
-    
+	const handleCloseForm = () => setShowingForm(false);
 
-  return (
-    <>
-    <buton class="add" ><img src={addButton} width={30} height={30}/></buton>
-    <div class="grid-container">
+	return (
+		<>
+			
+			<div class="grid-container">
+				{ingredients.map(ingredients => (
+					<div class="item-container" key={ingredients._id}>
+						<IngredientImg type={ingredients.category}/>
+						<h3 class="name">{ingredients.name}</h3>
+						<p class="courier">{ingredients.category}</p>
+						<p class="courier">{ingredients.calories} kcal/100g</p>
+					</div>
+				))}
+				
+				<buton class="add" onClick={handleShowForm}><img src={addButton} width={30} height={30}/></buton>
+				<IngredientForm show={showingForm} handleClose={handleCloseForm}/>
 
-      {ingredients.map(ingredients => (
-        <div class="item-container" key={ingredients._id}>
-          <IngredientImg type={ingredients.category}/>
-          <h3 class="name">{ingredients.name}</h3>
-          <p class="courier">{ingredients.category}</p>
-          <p class="courier">{ingredients.calories} kcal/g</p>
-          
-        </div>
-      ))}
-    </div><p class="end-prompt">You Reach The End</p></>
-    
-  );
+			</div>
+
+			<p class="end-prompt">You Reach The End</p>
+		</>
+	
+	);
 }
 
 const IngredientImg = (props) => {
 
-  let img = ""
+	let img = ""
 
-  switch (props.type){
-    case 'Carbohydrate' :
-      img = carb;
-      break;
-    case 'Protein' :
-      img = protein;
-      break;
-  case 'Vegetable' :
-      img = vege
-      break;
-     case 'Fat' :
-      img = fat;
-      break;
-     case 'Fruit' :
-      img = fruit;
-      break;
-     default : 
-      img = random;
-  }
+	switch (props.type){
+		case 'Carbohydrate' :
+			img = carb;
+		break;
+		case 'Protein' :
+			img = protein;
+			break;
+		case 'Vegetable' :
+			img = vege
+			break;
+		case 'Fat' :
+			img = fat;
+			break;
+		case 'Fruit' :
+			img = fruit;
+			break;
+		default : 
+			img = random;
+	}
 
-  console.log(img)
+	console.log(img)
 
-  return (
-    <img src={img} height = {200} width = {200}></img>
-  )
+	return (
+		<img src={img} height = {200} width = {200}></img>
+	)
 }
+
+const handleClick = () => {
+	  // Logic to execute when the button is clicked
+	  console.log('Button clicked!');
+	
+}  
