939eccf02f6786f2918ec476feac9b3a329c8d4a
JohnSalinas123 <salinasjohn257@gmail.com>
2023-12-07 22:23:39 -0800

    add socket server, for websockets

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,25 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 import userRoutes from './routes/userRoutes.js';
 import ingredientRoutes from './routes/ingredientRoutes.js';
 import recipeRoutes from './routes/recipeRoutes.js';
-import {dbConnect} from './dbConnect.js'
+import {dbConnect} from './dbConnect.js';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
+const expressPort = 8080;
+const socketPort = 3000;
 const app = express()
-const port = 8080;
+const httpServer = createServer(app)
+
+const io = new Server(httpServer, {})
+
+io.on("connection", (socket) => {
+    console.log(`Socket server running on port ${socketPort}`)
+
+
+})
 
 // connect to mongodb atlas
 dbConnect();
@@ -24,9 +36,13 @@ app.use('/api/user', userRoutes)
 //app.use('/api/ingredients', ingredientRoutes)
 //app.use('/api/recipes', recipeRoutes)
 
-app.listen(port, () => {
-    console.log(`Server is running at port ${port}`)
+app.listen(expressPort, () => {
+    console.log(`Express server is running at port ${expressPort}`)
 })
 
+
+httpServer.listen(socketPort, () => {
+    console.log(`Socket server running on port ${socketPort}`)
+})
 
 
